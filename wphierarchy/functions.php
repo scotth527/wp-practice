1e7c3dda568fe4ac59d23affe03047c40a606ee2
scotth527 <scottyh527@gmail.com>
2021-06-22 20:58:07 -0400

    added more filters

--- wphierarchy/functions.php
+++ wphierarchy/functions.php
@@ -124,6 +124,29 @@ add_filter( 'excerpt_length', 'wphooks_excerpt_length' , 20 );
 $exceprt_length = apply_filters( 'excerpt_length', 55 );
 
 
+//Redirect login
+function wphooks_members_login_redirect( $redirect_to, $request, $user ) {
+    if( isset( $user->roles ) && is_array( $user->roles ) ) {
+        if( !in_array( 'administrator', $user->roles) ) {
+            return home_url( '/members' );
+        } else {
+            return $redirect_to;
+        }
+    }
+
+    return;
+}
+
+add_filter( 'login_redirect', 'wphooks_members_login_redirect', 10, 3 );
+
+//Filter caldera forms
+function wphooks_change_caldera_button_class( $field_html ) {
+    $new_field_html = str_replace( 'btn', 'button', $field_html );
+    return $new_field_html;
+
+}
+
+add_filter( 'caldera_forms_render_field_type-button', 'wphooks_change_caldera_button_class', 10, 1 );
 
 //Add theme support
 
